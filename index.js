5832eb4132073f9cfc86edfa29f575ef56cc3352
Gustavo Costa <cmtehenz@gmail.com>
2018-08-29 23:33:48 -0300

    add pug

--- index.js
+++ index.js
@@ -6,10 +6,16 @@ const methodOverride = require('method-override')
 const app = express()
 
 app.use(morgan('dev'))
-app.use(bodyParser.urlencoded({ extended: false}))
+app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 app.use(methodOverride('_method'))
 
-app.listen(3000, () => {
-    console.log('Express has been ignite')
+app.set('view engine', 'pug')
+app.set('views', path.join(__dirname, 'src/view'))
+
+//require('./src/model/index')
+require('./src/index')(app)
+
+app.listen(9000, () => {
+	console.log('Express has been ignite')
 })
